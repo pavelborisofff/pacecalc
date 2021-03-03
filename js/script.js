2f81a00e8c2e847a78cdf1ae1ed62a4a82183b77
pavelborisov <pavel.borisofff@gmail.com>
2021-03-04 00:31:19 +0300

    working speed and pace

--- js/script.js
+++ js/script.js
@@ -12,7 +12,21 @@ let pace = 5 * 60 / 1000,  // seconds per meter
     time = pace * distance; // seconds
 
 function setMaxLength(value, l = 2) {
-    return (value > 9) ? value : '0'.repeat(l - 1) + value;
+    // return (value > 9) ? value : '0'.repeat(l - 1) + value;
+    return value;
+}
+
+function getPace() {
+    console.log(paceFields[0].value, paceFields[1].value);
+    return (paceFields[0].value * 60 + paceFields[1].value * 1) / 1000;
+}
+
+function getSpeed() {
+    return parseFloat(speedFields[0].value + paceFields[1].value) * 1000 / (60 * 60);
+}
+
+function getTime() {
+    return parseFloat(timeFields[0].value * 60 * 60 + timeFields[1].value * 60 + timeFields[2].value);
 }
 
 function setPace(field) {
@@ -87,6 +101,7 @@ function getDistance()  {
 
 function bindListener(field) {
     field.addEventListener('input', (event) => {
+        console.log(event.target.value);
         switch (event.target.name) {
             case 'distance':
                 distance = getDistance();
@@ -95,6 +110,7 @@ function bindListener(field) {
                 break;
             case 'pace':
                 pace = getPace();
+                console.log(pace);
                 speed = 1 / pace;
                 time = Math.ceil(pace * distance);
                 break;
